a56d681b61e3fafb92befec32c9bb2b96516785b
Shokhrukh <wiut00013674@gmail.com>
2023-03-30 16:05:11 +0500

    new changes

---
+++ CW2_beta/README.md
@@ -0,0 +1,28 @@
+# Cards web application
+
+### Users are able to add, delete, and update cards using this web program
+
+You must perform the actions listed below in order to execute this web application locally
+
+1. Clone this reporitory
+```bash
+git clone [repository link]
+```
+
+2. Install dependencies
+```bash
+cd [project folder]
+npm install
+```
+
+3. Run the application
+```bash
+node app
+```
+
+### Web application dependencies
+ - express.js
+ - pug.js
+
+### Web application repository on github 
+[link to github repo]
---
+++ CW2_beta/app.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+// localhost:8000
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/create', (req, res) =>{
+    res.render('create')
+})
+
+app.post('/create', (req, res) =>{
+    const title = req.body.title
+    const description = req.body.description
+    
+    if(title.trim() === '' && description.trim() === ''){
+        res.render('create', { error: true })
+    } else {
+        fs.readFile('./data/cards.json', (err, data) =>{
+            if(err) throw err
+
+            const cards = JSON.parse(data)
+
+            cards.push({
+                id: id (),
+                title: title,
+                description: description,
+            })
+
+            fs.writeFile('./data/cards.json', JSON.stringify(cards), err => {
+                if(err) throw err
+
+                res.render('create', { success: true })
+            })
+        })
+    }
+
+})
+
+
+app.get('/cards', (req, res) => {
+    
+    fs.readFile('./data/cards.json', (err, data) => {
+      if (err) throw err
+
+      const cards = JSON.parse(data)
+
+      res.render('cards', { cards: cards })
+    })
+})
+
+app.get('/cards/:id', (req, res) =>{
+    const id = req.params.id
+
+    fs.readFile('./data/cards.json', (err, data) => {
+        if (err) throw err
+  
+        const cards = JSON.parse(data)
+  
+        const card = cards.filter(card => card.id == id)[0]
+
+        res.render('detail', { card: card })
+     })
+})
+
+app.listen(8000, err => {
+    if (err) console.log(err)
+
+    console.log('Server is running on port 8000...')
+})
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
